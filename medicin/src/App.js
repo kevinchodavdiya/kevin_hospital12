c465a08c033543ac67c3a5b900bdbb1106f5d1c3
kevinchodavdiya <kevinchodavdiya4541@gmail.com>
2022-06-10 08:31:29 +0530

    done

--- medicin/src/App.js
+++ medicin/src/App.js
@@ -1,18 +1,22 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Layout from './componet/layout/layout';
+import Layout from './Components/Layout/Layout';
 import Doctor from './container/doctor/Doctor';
 import Medicine from './container/Medicine/Medicine';
 
 function App() {
   return (
     <>
+     
+
     <Layout>
       <Switch>
-        <Route exact path={"/medicine"} component={Medicine}/>
+        <Route exact path={"/medicine"} component={Medicine} />
         <Route exact path={"/doctor"} component={Doctor}/>
       </Switch>
     </Layout>
+
+    
     </>
   );
 }
